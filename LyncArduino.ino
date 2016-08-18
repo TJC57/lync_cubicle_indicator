f18eb704f6d36fc2653675006804ec524bfaa184
TJC57 <tim.chen57@gmail.com>
2016-08-18 16:41:00 -0600

    Initial Upload; Lync Status w/ Arduino & PowerShell
    This is the initial upload.  It currently attempts to set the status on an LED and a NeoPixel LED.  The LED status works fine, but the NeoPixel LED needs tuning.  For project delivery, the NeoPixel is a want but not necessarily a must.

---
+++ LyncArduino.ino
@@ -0,0 +1,137 @@
+#include <Adafruit_NeoPixel.h>
+// https://www.youtube.com/watch?v=g0pSfyXOXj8
+
+#ifdef __AVR__
+  #include <avr/power.h>
+#endif
+
+const int neoPixelPin = 8;
+const int greenPin = 9;
+const int yellowPin = 10;
+const int redPin = 11;
+Adafruit_NeoPixel strip = Adafruit_NeoPixel(1,neoPixelPin,NEO_RGB);
+
+int boardPin = 13;
+
+void setup() {
+  #if defined (__AVR_ATtiny85__)
+    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
+  #endif
+  Serial.begin(9600);
+
+  pinMode(boardPin, OUTPUT);
+  pinMode(greenPin, OUTPUT);
+  pinMode(yellowPin, OUTPUT);
+  pinMode(redPin, OUTPUT);
+
+  // Initialize NeoPixel
+  strip.begin();
+  strip.setPixelColor(0, 0, 0, 0);
+  strip.show();
+}
+
+void loop() {
+  // Have the arduino wait to receive input
+  // strip.setPixelColor(NeoPixel#,Red,Green,Blue)
+  // Range is from 0-255
+  while(Serial.available() == 0);
+
+  // Read the input (ascii)
+  //int val = Serial.read() - '0';
+  int val = Serial.read();
+
+  switch (val){
+    case 'f':
+      Serial.println("Lync Status: Free");
+      strip.setPixelColor(0,0,255,0);
+      strip.show();
+      delay(100);
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, LOW);
+      break;
+    case 'a':
+      Serial.println("Lync Status: Away");
+      strip.setPixelColor(0,180,250,0);
+      strip.show();
+      delay(100);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, HIGH);
+      digitalWrite(redPin, LOW);
+      break;
+    case 'b':
+      Serial.println("Lync Status: Busy");
+      strip.setPixelColor(0,255,0,0);
+      strip.show();
+      delay(100);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, HIGH);
+      break;
+    case 'd':
+      Serial.println("Lync Status: Do Not Disturb");
+      strip.setPixelColor(0,153,0,0);
+      strip.show();
+      delay(100);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, HIGH);
+      break;
+    case 'o':
+      Serial.println("Lync Status: Offline");
+      strip.setPixelColor(0,0,0,0);
+      strip.show();
+      delay(100);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, LOW);
+      digitalWrite(redPin, LOW);
+      break;
+    case 'r':
+      Serial.println("Lync Status: Be Right Back");
+      strip.setPixelColor(0,128,255,0);
+      strip.show();
+      delay(100);
+      digitalWrite(greenPin, HIGH);
+      digitalWrite(yellowPin, HIGH);
+      digitalWrite(redPin, LOW);
+      break;
+    case 'i':
+      Serial.println("Lync Status: Busy Idle");
+      strip.setPixelColor(0,255,153,51);
+      strip.show();
+      delay(100);
+      digitalWrite(greenPin, LOW);
+      digitalWrite(yellowPin, HIGH);
+      digitalWrite(redPin, HIGH);
+      break;
+    case '1':
+      Serial.println("Board Led is On");
+      digitalWrite(boardPin, HIGH);
+      break;
+    case '0':
+      Serial.println("Board Led is Off");
+      digitalWrite(boardPin, LOW);
+      break;
+    default:
+      Serial.print("Invalid Entry!  You entered: ");
+      Serial.println(val);
+  }
+
+/*
+  // get neopixel values
+  int red = 0;
+  int blue = 0;
+  int green = 0;
+  red, green, blue = strip.getPixelColor(0);
+  Serial.print("Red: ");
+  Serial.print(red);
+  Serial.print(" Green: ");
+  Serial.print(green);
+  Serial.print(" Blue: ");
+  Serial.println(blue);
+ */
+  
+  strip.show();
+  Serial.flush();
+
+}
---
+++ LyncArduino.ps1
@@ -0,0 +1,167 @@
+﻿# Lync Status Arduino via PowerShell script
+# Written by Tim Chen
+
+# Referenced Sources:
+# http://www.ravichaganti.com/blog/finding-lync-contact-availability-using-powershell/
+
+
+$prev = 'o'
+$hosebreak = 0
+
+if(-not (Get-Module -Name Microsoft.Lync.Model)){
+    try{
+        Import-Module -Name (Join-Path -Path ${env:ProgramFiles(x86)} -ChildPath "Microsoft Office\Office15\LyncSDK\Assemblies\Desktop\Microsoft.Lync.Model.dll") -ErrorAction Stop
+    }
+    catch{
+        Write-Warning "Microsoft.Lync.Model cannot be found, install LyncSDK.exe"
+        break
+    }
+}
+
+try{
+    # Initialize the Serial Port Settings
+    $port = New-Object System.IO.Ports.SerialPort
+    $port.PortName = "COM6"
+    $port.BaudRate = "9600"
+    $port.Parity = "None"
+    $port.DataBits = 8
+    $port.StopBits = 1
+    $port.ReadTimeout = 2000 # 5 seconds
+    #$port.DtrEnable = "true
+}
+catch [System.IO.IOException]
+{
+    Write-Warning "$(Get-Date -Format "[MM-dd-yyyy hh:mm:ss]") Cannot access COM port, please check connection!"
+    break
+}
+
+while($true){
+    try{
+        $client = [Microsoft.Lync.Model.LyncClient]::GetClient()
+    }
+    catch [Microsoft.Lync.Model.ClientNotFoundException]
+    {
+        Write-Warning "$(Get-Date -Format "[MM-dd-yyyy hh:mm:ss]") Lync Client not found!"
+        $char = 'o'
+    }
+    catch [Microsoft.Lync.Model.NotStartedByUserException]
+    {
+        Write-Warning "$(Get-Date -Format "[MM-dd-yyyy hh:mm:ss]") Lync/Skype for Business is not open.  Please open it."
+        break
+    }
+
+    # Check if client signed in.
+    if($client.state -eq "SignedIn"){
+        # Create a Lync contact object on yourself
+        $contact = $client.ContactManager.GetContactByUri("tim.j.chen@hp.com")
+        # Retrieve the Lync Status with the correct enumeration.
+        # Enumeration is found at https://msdn.microsoft.com/en-us/library/microsoft.lync.controls.contactavailability_di_2_lyncctrlslmref.aspx
+        $status = [Microsoft.Lync.Model.ContactAvailability] $contact.GetContactInformation("Availability")
+        #Write-Output "My Lync Status is: " $status
+    
+        # Map Lync Statuses with integers for Arduino Sketch Handling
+        switch ($status){
+            None {
+                "PS Lync Status: None"
+                 $char = 'o'   
+                 break
+            }
+            Free {
+                "PS Lync Status: Free"
+                 $char = 'f'     
+                 break
+            }
+            FreeIdle {
+                "PS Lync Status: FreeIdle" 
+                 $char = 'r'
+                 break
+            }
+            Busy {
+                "PS Lync Status: Busy"
+                $char = 'b'
+                break
+            }
+            BusyIdle {
+                "PS Lync Status: BusyIdle"
+                $char = 'i'
+                break
+            }
+            DoNotDisturb {
+                "PS Lync Status: Do Not Disturb"
+                $char = 'd'
+                break
+            }
+            TemporarilyAway {
+                "PS Lync Status: Be Right Back"
+                $char = 'r'
+                break
+            }
+            Away {
+                "PS Lync Status: Away"
+                $char = 'a'    
+                break
+            }
+            Offline {
+                "PS Lync Status: Offline"
+                $char = 'o'
+                break
+            }
+            default {"Invalid Lync Status!!"; break}
+        }
+
+    }
+    else{
+        Write-Warning "$(Get-Date -Format "[MM-dd-yyyy hh:mm:ss]") Lync is not running, or you are not signed in."
+        $char = 'o'
+        Start-Sleep 1
+    }
+
+    # Check if the Arduino is connected (verify hosebreak scenario)
+    try
+    {
+        #$port = New-Object System.IO.Ports.SerialPort COM6,9600,None,8,one
+        # Source: http://forum.arduino.cc/index.php?topic=39804.0
+        if (!$port.IsOpen){
+            try{
+                $port.open()
+                if ($prev -ne $char)
+                {
+                    $port.WriteLine($char)
+                    $port.ReadLine()
+                }
+                elseif ($prev -eq $char -and $hosebreak -eq 1)
+                {
+                    $port.WriteLine($char)
+                    $port.ReadLine()
+                    $hosebreak = 0
+                }
+                # update previous character
+                $prev = $char
+            }
+            catch [System.UnauthorizedAccessException]
+            {
+                Write-Warning "$(Get-Date -Format "[MM-dd-yyyy hh:mm:ss]") Access to COM port denied, check for other instances & wait..."
+                Start-Sleep 1
+            }
+            catch [System.TimeoutException]
+            {
+                Write-Warning "$(Get-Date -Format "[MM-dd-yyyy hh:mm:ss]") ReadLine timed out, continue script to recover..."
+            }
+            finally{
+                $port.close()
+                Write-Output "$(Get-Date -Format "[MM-dd-yyyy hh:mm:ss]") Closed Port"
+            }
+        }
+        else{
+            Write-Warning "$(Get-Date -Format "[MM-dd-yyyy hh:mm:ss]") Port is NOT open, continuing script..."
+        }
+    }
+    catch [System.IO.IOException]
+    {
+        Write-Warning "$(Get-Date -Format "[MM-dd-yyyy hh:mm:ss]") Cannot access COM port, please check connection!"
+        $hosebreak = 1
+    }
+
+    # wait 1s before checking again
+    Start-Sleep 1
+}
